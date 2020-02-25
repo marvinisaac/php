f629cbbc23c2b372a44672e702a58c1ad857bdf7
Marvin Isaac <marvin@marvinisaac.com>
2020-02-25 03:39:46 +0000

    :hammer: Remove timestamp in image version

--- build.sh
+++ build.sh
@@ -2,9 +2,8 @@
 
 docker login -u marvinisaac -p $BUILD_SECRET docker.pkg.github.com
 
-version=1.0.0
-timestamp=$(date +%s)
-tag=docker.pkg.github.com/marvinisaac/php/php-7.4:$version-$timestamp
+version=1.0.1
+tag=docker.pkg.github.com/marvinisaac/php/php-7.4:$version
 
 echo 'Building custom PHP image...'
 sleep 1
